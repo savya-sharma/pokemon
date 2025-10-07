c401ecaaa6ba44a997a026d9e1523f6ae36f7dfa
savya-sharma <savya.sharma@example.com>
2025-10-07 23:08:15 +0530

    pending

--- script.js
+++ script.js
@@ -8,21 +8,22 @@ async function fetchData() {
     // console.log(images)
 
     for (let i = 0; i < names.length; i++) {
+        let card = document.createElement("div")
         let heading = document.createElement("h3")
+        card.append(heading)
         heading.textContent = names[i].name
-        wrapper.append(heading)
 
+        card.classList.add("card")
         let images = result.results[i].url
         let imgs = await fetch(images);
         let imageData = await imgs.json();
-        console.log(imageData)
-
         let getImage = imageData.sprites.other.dream_world.front_default;
         console.log(getImage)
-
         let image = document.createElement("img")
+        image.classList.add("img-div")
         image.src = getImage
-        wrapper.append(image)
+        card.append(image)
+        wrapper.append(card)
 
     }
 }
