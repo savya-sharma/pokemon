34b155552d5d26f5e763cd75f7d85cc54c561f0b
savya-sharma <savya.sharma@example.com>
2025-10-08 21:32:44 +0530

    still in action

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let main = document.querySelector("main")
-let wrapper = document.querySelector("#wrapper")
+const main = document.querySelector("main")
+const wrapper = document.querySelector("#wrapper")
+const loadMoreBtn = document.querySelector("button")
+const bySearch = document.querySelector("#searchByName")
 let url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
 let promises = [];
+let finalData = [];
+let offset = 0;
 
 
 
@@ -17,41 +21,74 @@ window.addEventListener("load", async () => {
     let promise = await fetchData.json();
     let data = promise.results;
 
+
     for (let i = 0; i < data.length; i++) {
-        let heading = document.createElement("h3")
-        heading.textContent = data[i].name
-        console.log(heading)
-
-        function againFetch() {
-            let imagePath = data[i].url
-            
-            console.log(imagePath)
-        }
-
-        let imageDiv = document.createElement("div")
-        let img = document.createElement("img")
-        img.src = againFetch();
+        promises.push(fetchUrl(data[i].url))
     }
-    // againFetch();
-    console.log(data)
+    finalData.push(...await Promise.all(promises))
+    // console.log(finalData)
 
+
+    displayData(finalData);
 })
 
 
-// async function againFetch() {
-//     let
+function displayData(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let card = document.createElement("div")
+        card.classList.add("card")
+        let heading = document.createElement("h3")
+        heading.textContent = arr[i].name
+        
+        let image = arr[i].sprites.other.dream_world.front_default;
+        let imageDiv = document.createElement("div")
+        imageDiv.classList.add("img-div")
+        let img = document.createElement("img")
+        img.src = image
+        imageDiv.append(img)
+        card.append(imageDiv)
+        main.append(card)
+        card.append(heading);
+    }
+}
+
+
+// function loadMore() {
+loadMoreBtn.addEventListener("click", () => {
+    offset = offset + 20;
+    let offsetUrl = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`;
+    // console.log(offsetUrl)
+
+    async function fetchAgain() {
+        main.innerHTML = ""
+        let morePokemonFetch = await fetch(offsetUrl)
+        let moreResponse = await morePokemonFetch.json();
+        let responseResult = moreResponse.results;
+        console.log(responseResult)
+
+        const newPromises = [];
+        for (let i = 0; i < responseResult.length; i++) {
+            newPromises.push(fetchUrl(responseResult[i].url))
+        }
+        const newData = await Promise.all(newPromises);
+        finalData.push(...newData)
+
+        displayData(finalData)
+    }
+    fetchAgain();
+})
 // }
+// loadMore();
 
 
-
-
-
-
-
-
-
-
-
+bySearch.addEventListener("keyup", (e) => {
+    const searchvalue = e.target.value.toLowerCase();
+    const filteredData = finalData.filter(pokemon => 
+        pokemon.name.toLowerCase().includes(searchvalue)   
+    )
+    main.innerHTML = ""
+    displayData(filteredData)
+})
 
 
 
